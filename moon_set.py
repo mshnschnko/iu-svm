6868156f1d7d3c5d128d773c9ac188be02880e5d
Mikhail Anishchenko <misha321654897i@gmail.com>
2024-03-23 14:01:44 +0300

    some updates

--- moon_set.py
+++ moon_set.py
@@ -1,6 +1,8 @@
 import os
 import threading
+import multiprocessing
 import numpy as np
+from numpy import ndarray
 from scipy.stats import multivariate_normal
 from matplotlib import pyplot as plt
 from tqdm import tqdm
@@ -8,7 +10,7 @@ from copy import deepcopy
 from sklearn.datasets import make_moons
 
 from IU_SVM import IUSVMClassifier
-from Kernel import GaussKernel, LinearKernel, PolyKernel
+from Kernel import GaussKernel, LinearKernel, PolyKernel, Kernel
 
 
 # def linKernel(x1, x2):
@@ -17,6 +19,133 @@ from Kernel import GaussKernel, LinearKernel, PolyKernel
 # def othKernel(x1, x2, sigma):
 #     return np.exp(-np.dot(x1 - x2, x1 - x2) / (2 * sigma*sigma))
 
+def classify(dataset: ndarray, train_size: int, kernel: Kernel, C: float, size: int, dir: str):
+    data = deepcopy(dataset)
+    sample1 = []
+    sample2 = []
+    sample = []
+    color = []
+
+    min_x = 1e12
+    max_x = -1e12
+    min_y = 1e12
+    max_y = -1e12
+
+    classifier = IUSVMClassifier(kernel, C)
+
+    for i, point in enumerate(data):
+        min_x = min(min_x, point[0])
+        max_x = max(max_x, point[0])
+        min_y = min(min_y, point[1])
+        max_y = max(max_y, point[1])
+        if point[2] == 0.0:
+            sample1.append(point)
+            sample.append(point)
+            color.append("red")
+        elif point[2] == 1.0:
+            sample2.append(point)
+            sample.append(point)
+            color.append("green")
+
+    # train_size = 300
+    train = data[:train_size]
+    test = data[train_size:]
+
+    for i, point in enumerate(train):
+        if color[i] == "red":
+            # print(point[:2])
+            classifier.addDataPair(point[:2], 1)
+        else:
+            classifier.addDataPair(point[:2], -1)
+
+    plt.figure()
+
+    delta_x = (max_x - min_x) / 100
+    delta_y = (max_y - min_y) / 100
+
+    x = np.arange(min_x, max_x, delta_x)
+    y = np.arange(min_y, max_y, delta_y)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    for i, y_val in enumerate(y):
+        for j, x_val in enumerate(x):
+            Z[i,j] = classifier([x_val,y_val])
+
+    plt.contour(X,Y,Z, levels=[-1,0,1], colors=['grey','k','grey'])
+
+    sampleS = np.array([data[i] for i in classifier.S])
+    colorS = [color[i] for i in classifier.S]
+    sampleO = np.array([data[i] for i in classifier.O])
+    colorO = [color[i] for i in classifier.O]
+    sampleE = np.array([data[i] for i in classifier.E])
+    colorE = [color[i] for i in classifier.E]
+
+    testO = []
+    testE = []
+    colorTestO = []
+    colorTestE = []
+    for j, point in enumerate(test):
+        i = train_size + j
+        val = 0
+        col = None
+        if color[i] == 'red':
+            col = 'm'
+            val = 1
+        if color[i] == 'green':
+            col = 'c'
+            val = -1
+        if classifier(point[:2]) * val < 0:
+            testE.append(point[:2])
+            colorTestE.append(col)
+        else:
+            testO.append(point[:2])
+            colorTestO.append(col)
+
+    testO = np.array(testO)
+    testE = np.array(testE)
+
+    plt.title(f'{kernel}, $C = {C}$')
+    if len(sampleS) != 0:
+        plt.scatter(sampleS[:,0], sampleS[:,1], c=colorS, marker="d", s=128)
+    if len(sampleO) != 0:
+        plt.scatter(sampleO[:,0], sampleO[:,1], c=colorO, marker=".", s=24)
+    if len(sampleE) != 0:
+        plt.scatter(sampleE[:,0], sampleE[:,1], c=colorE, marker="+", s=64)
+    if len(testO) != 0:
+        plt.scatter(testO[:,0], testO[:,1], c=colorTestO, marker=".", s=24)
+    if len(testE) != 0:
+        plt.scatter(testE[:,0], testE[:,1], c=colorTestE, marker="+", s=64)
+
+    dirname = os.path.join(dir, kernel.filename)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    fig_filename = os.path.join(dirname, f'{kernel.filename}_C_{C}.png')
+    txt_filename = os.path.join(dirname, f'{kernel.filename}_C_{C}.txt')
+    plt.savefig(fig_filename)
+    
+    miss = 0
+    for i, point in enumerate(data):
+        val = 0
+        if color[i] == 'red':
+            val = 1
+        if color[i] == 'green':
+            val = -1
+        if classifier(point[:2]) * val < 0:
+            miss += 1
+        
+    # print('=========== ЛИНЕЙНО НЕРАЗДЕЛИМЫЕ ДАННЫЕ ===========')
+    # print('|S| = ', len(classifier.S))
+    # print('|E| = ', len(classifier.E))
+    # print('|R| = ', len(classifier.O))
+    # print('miss count = ', miss)
+    # print(miss / size)
+    with open(txt_filename, 'w') as f:
+        f.write(f'|S| = {len(classifier.S)}\n')
+        f.write(f'|E| = {len(classifier.E)}\n')
+        f.write(f'|R| = {len(classifier.O)}\n')
+        f.write(f'miss count = {miss}\n')
+        f.write(f'error (%) = {miss / size}\n')
+    # plt.show()
 
 def nonlinear_researh():
     size = 1000
@@ -43,153 +172,44 @@ def nonlinear_researh():
     data, labels = make_moons(n_samples=size, noise=0.2, random_state=42)
     dataset = np.hstack((data, labels.reshape(-1, 1)))
     np.random.shuffle(dataset)
-    # print(dataset)
-    # return
 
-
-    # print(len(dataset[:,2].where(dataset[:,2] == 0)))
-    # print(len(np.where(dataset[:,2] == 1.0)[0]))
-    # return
     # Визуализация данных
     plt.scatter(dataset[:, 0], dataset[:, 1], c=dataset[:, 2], cmap='viridis', s=10)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Два пересекающихся класса')
     plt.colorbar(label='Класс')
-    # plt.show()
 
-    # plt.figure()
-    # plt.scatter(orig_data[:,0], orig_data[:,1], s=10)
+    train_size = 300
+    timeout = 180
+    dirname = f'figures_tr{train_size}_to{timeout}'
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    fig_filename = os.path.join(dirname, f'data.png')
+    plt.savefig(fig_filename)
     # plt.show()
 
     C_list = [10, 100, 1000, 10000, 1000000]
-    kernels = [GaussKernel(sigma=1.0), GaussKernel(sigma=4.0), GaussKernel(sigma=8.0), PolyKernel(degree=2, c=1.0), PolyKernel(degree=3, c=1.0)]
-    # print(kernel)
+    kernels = [
+        GaussKernel(sigma=1.0),
+        GaussKernel(sigma=4.0),
+        GaussKernel(sigma=8.0),
+        PolyKernel(degree=2, c=1.0),
+        PolyKernel(degree=3, c=1.0),
+        PolyKernel(degree=5, c=1.0),
+    ]
+    
+    # grid search
     for kernel in tqdm(kernels):
         for C in C_list:
-            data = deepcopy(dataset)
-            sample1 = []
-            sample2 = []
-            sample = []
-            color = []
-
-            min_x = 1e12
-            max_x = -1e12
-            min_y = 1e12
-            max_y = -1e12
-
-            classifier = IUSVMClassifier(kernel, C)
-
-            for i, point in enumerate(data):
-                min_x = min(min_x, point[0])
-                max_x = max(max_x, point[0])
-                min_y = min(min_y, point[1])
-                max_y = max(max_y, point[1])
-                if point[2] == 0.0:
-                    sample1.append(point)
-                    sample.append(point)
-                    color.append("red")
-                elif point[2] == 1.0:
-                    sample2.append(point)
-                    sample.append(point)
-                    color.append("green")
-
-            for i, point in enumerate(data[:100]):
-                if color[i] == "red":
-                    # print(point[:2])
-                    classifier.addDataPair(point[:2], 1)
-                else:
-                    classifier.addDataPair(point[:2], -1)
-
-            plt.figure()
-
-            delta_x = (max_x - min_x) / 100
-            delta_y = (max_y - min_y) / 100
-
-            x = np.arange(min_x, max_x, delta_x)
-            y = np.arange(min_y, max_y, delta_y)
-            X, Y = np.meshgrid(x, y)
-            Z = np.zeros_like(X)
-            for i, y_val in enumerate(y):
-                for j, x_val in enumerate(x):
-                    Z[i,j] = classifier([x_val,y_val])
-
-            plt.contour(X,Y,Z, levels=[-1,0,1], colors=['grey','k','grey'])
-
-            sampleS = np.array([data[i] for i in classifier.S])
-            colorS = [color[i] for i in classifier.S]
-            sampleO = np.array([data[i] for i in classifier.O])
-            colorO = [color[i] for i in classifier.O]
-            sampleE = np.array([data[i] for i in classifier.E])
-            colorE = [color[i] for i in classifier.E]
-
-            testO = []
-            testE = []
-            colorTestO = []
-            colorTestE = []
-            for j, point in enumerate(dataset[100:]):
-                i = 100 + j
-                val = 0
-                col = None
-                if color[i] == 'red':
-                    col = 'm'
-                    val = 1
-                if color[i] == 'green':
-                    col = 'c'
-                    val = -1
-                if classifier(point[:2]) * val < 0:
-                    testE.append(point[:2])
-                    colorTestE.append(col)
-                else:
-                    testO.append(point[:2])
-                    colorTestO.append(col)
-
-            testO = np.array(testO)
-            testE = np.array(testE)
-
-            plt.title(f'{kernel}, $C = {C}$')
-            if len(sampleS) != 0:
-                plt.scatter(sampleS[:,0], sampleS[:,1], c=colorS, marker="d", s=128)
-            if len(sampleO) != 0:
-                plt.scatter(sampleO[:,0], sampleO[:,1], c=colorO, marker=".", s=24)
-            if len(sampleE) != 0:
-                plt.scatter(sampleE[:,0], sampleE[:,1], c=colorE, marker="+", s=64)
-            if len(testO) != 0:
-                plt.scatter(testO[:,0], testO[:,1], c=colorTestO, marker=".", s=24)
-            if len(testE) != 0:
-                plt.scatter(testE[:,0], testE[:,1], c=colorTestE, marker="+", s=64)
-
-            dirname = os.path.join('figures', kernel.filename)
-            if not os.path.isdir(dirname):
-                os.makedirs(dirname)
-            fig_filename = os.path.join(dirname, f'{kernel.filename}_C_{C}.png')
-            txt_filename = os.path.join(dirname, f'{kernel.filename}_C_{C}.txt')
-            plt.savefig(fig_filename)
+            p = multiprocessing.Process(target=classify, args=(dataset, train_size, kernel, C, size, dirname))
+            p.start()
+            p.join(timeout=timeout)
+            if p.is_alive():
+                print(f'TIMEOUT: {kernel} и C={C} выполняется слишком долго... завершение процесса...')
+                p.terminate()
+                p.join()
             
-            miss = 0
-            for i, point in enumerate(data):
-                val = 0
-                if color[i] == 'red':
-                    val = 1
-                if color[i] == 'green':
-                    val = -1
-                if classifier(point[:2]) * val < 0:
-                    miss += 1
-                
-            # print('=========== ЛИНЕЙНО НЕРАЗДЕЛИМЫЕ ДАННЫЕ ===========')
-            # print('|S| = ', len(classifier.S))
-            # print('|E| = ', len(classifier.E))
-            # print('|R| = ', len(classifier.O))
-            # print('miss count = ', miss)
-            # print(miss / size)
-            with open(txt_filename, 'w') as f:
-                f.write(f'|S| = {len(classifier.S)}\n')
-                f.write(f'|E| = {len(classifier.E)}\n')
-                f.write(f'|R| = {len(classifier.O)}\n')
-                f.write(f'miss count = {miss}\n')
-                f.write(f'error (%) = {miss / size}\n')
-        # plt.show()
-
 
 if __name__ == '__main__':
     nonlinear_researh()
